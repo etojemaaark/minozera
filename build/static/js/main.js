b9a53dd686bacec3d7984eea411ffcfc103ec298
etojemaaark <awfulmark@yandex.ru>
2021-03-02 14:51:54 +0500

    a

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -20,8 +20,8 @@ if (animItems.length > 0) {
   window.addEventListener('scroll', animOnScroll);
 
   function animOnScroll() {
-    for (var index = 0; index < animItems.length; index++) {
-      var animItem = animItems[index];
+    for (var _index = 0; _index < animItems.length; _index++) {
+      var animItem = animItems[_index];
       var animItemHeight = animItem.offsetHeight;
       var animItemOffset = offset(animItem).top;
       var animStart = 4;
@@ -62,4 +62,78 @@ var menu = document.querySelector('.header__menu-list');
 menuBurger.addEventListener('click', function () {
   menuBurger.classList.toggle('_active');
   menu.classList.toggle('_active');
+}); /// input mask ///
+
+var maskedInputs = document.querySelectorAll("[data-mask]");
+
+for (var index = 0; index < maskedInputs.length; index++) {
+  maskedInputs[index].addEventListener('input', maskInput);
+}
+
+function maskInput() {
+  var input = this;
+  var mask = input.dataset.mask;
+  var value = input.value;
+  var literalPattern = /[0\*]/;
+  var numberPattern = /[0-9]/;
+  var newValue = "";
+
+  try {
+    var maskLength = mask.length;
+    var valueIndex = 0;
+    var maskIndex = 0;
+
+    for (; maskIndex < maskLength;) {
+      if (maskIndex >= value.length) break;
+      if (mask[maskIndex] === "0" && value[valueIndex].match(numberPattern) === null) break; // Found a literal
+
+      while (mask[maskIndex].match(literalPattern) === null) {
+        if (value[valueIndex] === mask[maskIndex]) break;
+        newValue += mask[maskIndex++];
+      }
+
+      newValue += value[valueIndex++];
+      maskIndex++;
+    }
+
+    input.value = newValue;
+  } catch (e) {
+    console.log(e);
+  }
+} /// swiper /// 
+
+
+var swiper = new Swiper(".citys__slider", {
+  // navigation: {
+  // 	nextEl: '.swiper-button-next',
+  // 	prevEl: '.swiper-button-prev'
+  // },
+  pagination: {
+    el: '.citys__slider-progressbar',
+    type: 'progressbar'
+  },
+  // scrollbar: {
+  // 	el: '.citys__slider-scrollbar',
+  // 	draggable: true,
+  // },
+  slidesPerView: 1.1,
+  // spaceBetween: '28',
+  grabCursor: true,
+  // autoplay: {
+  // 	delay: 1000,
+  // 	disableOnInteraction: false,
+  // },
+  loop: true,
+  speed: 1000 // breakpoints: {
+  // 	1707: {
+  // 		slidesPerView: 3,
+  // 	},
+  // 	1440: {
+  // 		slidesPerView: 2,
+  // 	},
+  // 	0: {
+  // 		slidesPerView: 1,
+  // 	}
+  // }
+
 });
--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -10,7 +10,6 @@ window.onscroll = function showHeader() {
 		}
 	}
 }
-
 /// anim scroll ///
 
 const animItems = document.querySelectorAll('._anim-items');
@@ -61,5 +60,79 @@ menuBurger.addEventListener('click', function () {
 
 
 
+/// input mask ///
 
+var maskedInputs = document.querySelectorAll("[data-mask]");
 
+for (var index = 0; index < maskedInputs.length; index++) {
+	maskedInputs[index].addEventListener('input', maskInput);
+}
+
+function maskInput() {
+	var input = this;
+	var mask = input.dataset.mask;
+	var value = input.value;
+	var literalPattern = /[0\*]/;
+	var numberPattern = /[0-9]/;
+	var newValue = "";
+	try {
+		var maskLength = mask.length;
+		var valueIndex = 0;
+		var maskIndex = 0;
+
+		for (; maskIndex < maskLength;) {
+			if (maskIndex >= value.length) break;
+
+			if (mask[maskIndex] === "0" && value[valueIndex].match(numberPattern) === null) break;
+
+			// Found a literal
+			while (mask[maskIndex].match(literalPattern) === null) {
+				if (value[valueIndex] === mask[maskIndex]) break;
+				newValue += mask[maskIndex++];
+			}
+			newValue += value[valueIndex++];
+			maskIndex++;
+		}
+
+		input.value = newValue;
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+/// swiper /// 
+
+const swiper = new Swiper(".citys__slider", {
+	// navigation: {
+	// 	nextEl: '.swiper-button-next',
+	// 	prevEl: '.swiper-button-prev'
+	// },
+	pagination: {
+		el: '.citys__slider-progressbar',
+		type: 'progressbar',
+	},
+	// scrollbar: {
+	// 	el: '.citys__slider-scrollbar',
+	// 	draggable: true,
+	// },
+	slidesPerView: 1.1,
+	// spaceBetween: '28',
+	grabCursor: true,
+	// autoplay: {
+	// 	delay: 1000,
+	// 	disableOnInteraction: false,
+	// },
+	loop: true,
+	speed: 1000,
+	// breakpoints: {
+	// 	1707: {
+	// 		slidesPerView: 3,
+	// 	},
+	// 	1440: {
+	// 		slidesPerView: 2,
+	// 	},
+	// 	0: {
+	// 		slidesPerView: 1,
+	// 	}
+	// }
+});
