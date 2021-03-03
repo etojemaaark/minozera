72277a046c76ef42beaf6859b40f1deda1602255
etojemaaark <awfulmark@yandex.ru>
2021-03-03 18:00:08 +0500

    a

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -137,3 +137,83 @@ var swiper = new Swiper(".citys__slider", {
   // }
 
 });
+$(document).ready(function () {
+  // Anchor arrow click
+  // smooth scroll to anchor tag
+  $('a[href*="#"]:not([href="#featured"])').click(function () {
+    // Check to see if anchor tag is not carousel's featured link
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+      if (target.length) {
+        // Smooth scroll to link section
+        $('html, body').animate({
+          scrollTop: target.offset().top + 50
+        }, 900);
+        return false;
+      }
+    }
+  }); //highlight navigation
+
+  $(window).scroll(function () {
+    var windowpos = $(window).scrollTop();
+    $('.sticky__sidebar-list').removeClass('active');
+    $('.sticky__progress-bar--circle').removeClass('active'); // if (windowpos > $('#content-item-1').offset().top) {
+    // 	$('nav li a').removeClass('active');
+    // 	$('a[href$="#top"]').addClass('active');
+    // } //windowpos
+
+    if (windowpos > $('#content-item-1').offset().top) {
+      $('.sticky__sidebar-list').removeClass('active');
+      $('a[href$="#content-item-1"]').addClass('active');
+      $('a[href$="#content-item-1"] .sticky__progress-bar--circle').addClass('active');
+    } //windowpos
+
+
+    if (windowpos > $('#content-item-2').offset().top) {
+      $('.sticky__sidebar-list').removeClass('active');
+      $('a[href$="#content-item-2"]').addClass('active');
+      $('a[href$="#content-item-2"] .sticky__progress-bar--circle').addClass('active');
+    } //windowpos
+
+
+    if (windowpos > $('#content-item-3').offset().top) {
+      $('.sticky__sidebar-list').removeClass('active');
+      $('a[href$="#content-item-3"]').addClass('active');
+      $('a[href$="#content-item-3"] .sticky__progress-bar--circle').addClass('active');
+    } //windowpos
+
+
+    if (windowpos > $('#content-item-4').offset().top) {
+      $('.sticky__sidebar-list').removeClass('active');
+      $('a[href$="#content-item-4"]').addClass('active');
+      $('a[href$="#content-item-4"] .sticky__progress-bar--circle').addClass('active');
+    } //windowpos
+
+
+    if (windowpos > $('#content-item-5').offset().top) {
+      $('.sticky__sidebar-list').removeClass('active');
+      $('a[href$="#content-item-5"]').addClass('active');
+      $('a[href$="#content-item-5"] .sticky__progress-bar--circle').addClass('active');
+    } //windowpos
+    // if (windowpos > $('#bottom').offset().top) {
+    // 	$('nav li a').removeClass('active');
+    // 	$('a[href$="#bottom"]').addClass('active');
+    // } //windowpos
+    // Scrollbar progression
+    // pixels scrolled from top
+
+
+    var scrollTop = $(window).scrollTop(),
+        // document height
+    docHeight = $(document).height(),
+        // window height
+    winHeight = $(window).height(),
+        // percent of document scrolled
+    scrollPercent = scrollTop / (docHeight - winHeight),
+        scrollPercentRounded = Math.round(scrollPercent * 100); // incement progress bar as user scrolls
+
+    $('.sticky__progress-bar--increment').css('height', scrollPercentRounded + '%');
+  });
+}); // on load
